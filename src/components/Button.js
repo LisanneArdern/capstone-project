18acc4142a99479d94d9b2578e0113065f773b18
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-07 16:43:29 +0200

    Edit button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,21 @@
+import styled from 'styled-components/macro'
+import PropTypes from 'prop-types'
 
-  import styled from 'styled-components/macro'
-  export default function Button () {
-    return <div>Button</div>
-  }
-  
+const ButtonStyled = styled.button`
+  padding: 0.35em 1.2em;
+  border-radius: 0.12em;
+  text-align: center;
+  background-color: ${props => (props.isActive ? '#ccc' : 'white')};
+`
+
+Button.propTypes = {
+  isActive: PropTypes.bool,
+  children: PropTypes.node,
+  onClick: PropTypes.func.isRequired,
+}
+
+function Button(props) {
+  return <ButtonStyled {...props} />
+}
+
+export default Button
--- src/components/Button.spec.js
+++ src/components/Button.spec.js
@@ -1,11 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Button from './{name}'
 
-  import{render, screen} from '@testing-library/react'
-  import Button from './{name}'
-  
-  describe ('Button', () => {
-    it('renders', () => {
-      render(<Button />)
-      expect(screen.getByText('Button')).toBeInTheDocument()
-    })
-  }) 
-  
+describe('Button', () => {
+  it('contains a text', () => {
+    render(<Button onClick={() => {}}>Back</Button>)
+
+    const button = screen.getByRole('button', { name: 'Back' })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('calls onClick', () => {
+    const handleClick = jest.fn()
+    render(<Button onClick={handleClick}>Back</Button>)
+
+    const button = screen.getByRole('button', { name: 'Back' })
+    userEvent.click(button)
+
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+})
