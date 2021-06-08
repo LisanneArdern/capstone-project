8b3719cc6c18882fe8910ae0b2fe2fd011c6910a
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-08 10:44:34 +0200

    Add content to DetailedView.js

--- src/components/DetailedView.js
+++ src/components/DetailedView.js
@@ -1,6 +1,27 @@
-
-  import styled from 'styled-components/macro'
-  export default function DetailedView () {
-    return <div>DetailedView</div>
-  }
-  
+import styled from 'styled-components/macro'
+export default function DetailedView({
+  name,
+  botanicalName,
+  sun,
+  spread,
+  rowSpace,
+  details,
+}) {
+  return (
+    <section>
+      <h2>{name}</h2>
+      <dl>
+        <dt>Botanical Name:</dt>
+        <dd>{botanicalName}</dd>
+      </dl>
+      <h3>Quick Guide</h3>
+      <ul>
+        <li>{sun}</li>
+        <li>{spread}</li>
+        <li>{rowSpace}</li>
+      </ul>
+      <h3>Details</h3>
+      <p>{details}</p>
+    </section>
+  )
+}
