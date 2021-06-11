27257413c2069317998a70bc1f24bd752f146108
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-11 10:56:57 +0200

    Add Test for CropListPage

--- src/pages/CropListPage.spec.js
+++ src/pages/CropListPage.spec.js
@@ -1,11 +1,56 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import CropListPage from './CropListPage'
 
-  import{render, screen} from '@testing-library/react'
-  import CropListPage from './{name}'
-  
-  describe ('CropListPage', () => {
-    it('renders', () => {
-      render(<CropListPage />)
-      expect(screen.getByText('CropListPage')).toBeInTheDocument()
-    })
-  }) 
-  
+describe('CropListPage', () => {
+  it('renders two crops', () => {
+    render(
+      <CropListPage
+        onNavigate={jest.fn()}
+        crops={[
+          {
+            id: 1,
+            attributes: {
+              name: 'Banana',
+              main_image_path: 'image',
+            },
+          },
+          {
+            id: 2,
+            attributes: {
+              name: 'Apple',
+              main_image_path: 'image',
+            },
+          },
+        ]}
+      />
+    )
+
+    expect(screen.getByText('Banana')).toBeInTheDocument()
+    expect(screen.getByText('Apple')).toBeInTheDocument()
+    const imageListLength = screen.getAllByRole('img').length
+    expect(imageListLength).toBe(2)
+  })
+
+  it('calls onNavigate when clickling on crop', () => {
+    const handleClickDetails = jest.fn()
+    render(
+      <CropListPage
+        crops={[
+          {
+            id: 1,
+            attributes: {
+              name: 'Banana',
+              main_image_path: 'image',
+            },
+          },
+        ]}
+        onNavigate={handleClickDetails}
+      />
+    )
+
+    const Button = screen.getByRole('button')
+    userEvent.click(Button)
+    expect(handleClickDetails).toHaveBeenCalled()
+  })
+})
