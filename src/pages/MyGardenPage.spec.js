af4440673dcead413140571cf0a89469a585f2ca
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-17 22:24:36 +0200

    Add test for MyGardenPage using regular expression

---
+++ src/pages/MyGardenPage.spec.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import MyGardenPage from './MyGardenPage'
+
+describe('MyGardenPage', () => {
+  it('calls onClickDetails when clicking on the back button', () => {
+    const handleClickList = jest.fn()
+    const handleClickDetails = jest.fn()
+    render(
+      <MyGardenPage
+        favoriteIds={[{ id: '1a' }, { id: '2a' }]}
+        crops={[
+          {
+            id: 1,
+            attributes: {
+              name: 'Banana',
+              main_image_path: 'image',
+            },
+          },
+        ]}
+        onClickList={handleClickList}
+        onClickDetails={handleClickDetails}
+      />
+    )
+
+    const listButton = screen.getByText(/w*.\w*.list/gi)
+    userEvent.click(listButton)
+    expect(handleClickList).toHaveBeenCalled()
+  })
+})
