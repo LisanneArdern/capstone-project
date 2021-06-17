337a5e7406a6eeef827a9e9c9451720fc682230c
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-17 11:21:13 +0200

    Correct Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components/macro'
-import PropTypes from 'prop-types'
 
-const ButtonStyled = styled.button`
+const Button = styled.button`
   padding: 0.35em 1.2em;
   border: 0.1em solid black;
   border-radius: 1em;
@@ -9,17 +8,7 @@ const ButtonStyled = styled.button`
   margin: 0 0.3em 0.3em 0;
   font-weight: 300px;
   box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-  background-color: ${props => (props.isActive ? '#ccc' : 'white')};
+  background-color: ${props => (props.isFavorite ? '#ccc' : 'white')};
 `
-
-Button.propTypes = {
-  isActive: PropTypes.bool,
-  children: PropTypes.node,
-  onClick: PropTypes.func.isRequired,
-}
-
-function Button(props) {
-  return <ButtonStyled {...props} />
-}
 
 export default Button
