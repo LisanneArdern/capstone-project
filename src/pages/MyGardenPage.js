dcb172e51bb04ee0c045e409c3b66f5121fec3aa
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-17 15:13:43 +0200

    Add Header and Style to MyGardenPage

--- src/pages/MyGardenPage.js
+++ src/pages/MyGardenPage.js
@@ -1,25 +1,40 @@
 import styled from 'styled-components/macro'
 import Button from '../components/Button'
+import Header from '../components/Header'
 import CropItem from '../components/CropItem'
 
 export default function MyGardenPage({
-  favoriteCrops,
+  crops,
   onClickDetails,
   onClickList,
+  favoriteId,
 }) {
+  const favoriteCrops = crops.filter(crop =>
+    favoriteId.find(id => id === crop.id)
+  )
   return (
     <Wrapper>
-      <h1>My Garden</h1>
-      {favoriteCrops.map(({ id, attributes }) => (
-        <CropItem
-          key={id}
-          name={attributes.name}
-          image={attributes.main_image_path}
-          onClick={onClickDetails}
-        />
-      ))}
+      <Header>My Garden</Header>
+      <Container>
+        {favoriteCrops.map(({ id, attributes }) => (
+          <CropItem
+            key={id}
+            name={attributes.name}
+            image={attributes.main_image_path}
+            onClick={() => onClickDetails(id)}
+          />
+        ))}
+      </Container>
       <Button onClick={onClickList}>Back to List</Button>
     </Wrapper>
   )
 }
-const Wrapper = styled.section``
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: 48px auto 48px;
+  height: 100vh;
+`
+
+const Container = styled.div`
+  overflow-y: scroll;
+`
