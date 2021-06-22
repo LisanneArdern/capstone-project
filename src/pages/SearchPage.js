fd0a6868aee5315d059c89d5f5ac07655f763b92
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-22 15:46:35 +0200

    Add Styling to SearchPage

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import styled from 'styled-components'
 import Button from '../components/Button'
-import Header from '../components/Header'
 import CropItem from '../components/CropItem'
+import Header from '../components/Header'
 
 export default function SearchPage({
   crops,
@@ -15,40 +15,63 @@ export default function SearchPage({
   return (
     <Wrapper>
       <Header>Search</Header>
-      <div>
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchTerm}
-          onChange={handleChange}
-        />
-        <div>
-          {searchResults.map(({ id, attributes }) => (
-            <CropItem
-              key={id}
-              name={attributes.name}
-              image={attributes.main_image_path}
-              onClick={() => onClickDetails(id)}
-            />
-          ))}
-        </div>
-      </div>
+      <Input
+        placeholder="Search your favorite crops"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <Output>
+        {searchResults.length === 0 ? (
+          <>
+            {crops.map(({ id, attributes }) => (
+              <CropItem
+                key={id}
+                name={attributes.name}
+                image={attributes.main_image_path}
+                onClick={() => onClickDetails(id)}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {searchResults.map(({ id, attributes }) => (
+              <CropItem
+                key={id}
+                name={attributes.name}
+                image={attributes.main_image_path}
+                onClick={() => onClickDetails(id)}
+              />
+            ))}
+          </>
+        )}
+      </Output>
+
       <Button onClick={onClickFavorites}>My Garden</Button>
     </Wrapper>
   )
 
   function handleChange(event) {
     setSearchTerm(event.target.value)
-    const results = crops.filter(crop =>
-      crop.attributes.name.toLowerCase().includes(searchTerm)
+    const results = crops.filter(
+      crop =>
+        crop.attributes.name.toLowerCase().includes(searchTerm) ||
+        crop.attributes.name.includes(searchTerm)
     )
     setSearchResults(results)
-    console.log(searchResults)
   }
 }
 
 const Wrapper = styled.section`
   display: grid;
-  grid-template-rows: 48px auto 48px;
+  grid-template-rows: 48px 40px auto 48px;
   height: 100vh;
+`
+const Input = styled.input`
+  justify-self: center;
+  border: 0.1em solid #d3d3d3;
+  border-radius: 1em;
+  padding: 0.35em 1.2em;
+`
+const Output = styled.div`
+  overflow-y: auto;
 `
