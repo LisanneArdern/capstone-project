f8a97cffd3745727a7b2a8df957bd0f40633d2e0
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-23 12:36:39 +0200

    Improve test for SearchPage

--- src/pages/SearchPage.spec.js
+++ src/pages/SearchPage.spec.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import SearchPage from './SearchPage'
 
 describe('SearchPage', () => {
@@ -27,5 +28,62 @@ describe('SearchPage', () => {
     )
 
     expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+  it('renders two crops', () => {
+    render(
+      <SearchPage
+        onClickDetails={jest.fn()}
+        onClickFavorites={jest.fn()}
+        crops={[
+          {
+            id: 1,
+            attributes: {
+              name: 'Banana',
+              main_image_path: 'image',
+            },
+          },
+          {
+            id: 2,
+            attributes: {
+              name: 'Apple',
+              main_image_path: 'image',
+            },
+          },
+        ]}
+      />
+    )
+
+    expect(screen.getByText('Banana')).toBeInTheDocument()
+    expect(screen.getByText('Apple')).toBeInTheDocument()
+    const imageListLength = screen.getAllByRole('img').length
+    expect(imageListLength).toBe(2)
+  })
+  it('calls correct action when clicking on buttons', () => {
+    const handleClickDetails = jest.fn()
+    const handleClickFavorites = jest.fn()
+    render(
+      <SearchPage
+        crops={[
+          {
+            id: 1,
+            attributes: {
+              name: 'Banana',
+              main_image_path: 'image',
+            },
+          },
+        ]}
+        onClickDetails={handleClickDetails}
+        onClickFavorites={handleClickFavorites}
+      />
+    )
+
+    const buttons = screen.getAllByRole('button')
+    const detailsButton = buttons[0]
+    const favoriteButton = buttons[1]
+    userEvent.click(detailsButton)
+    expect(handleClickDetails).toHaveBeenCalled()
+
+    userEvent.click(favoriteButton)
+    expect(handleClickFavorites).toHaveBeenCalled()
   })
 })
