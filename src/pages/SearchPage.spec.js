75f42bc9f3d94472fc6c1f0ee26a7935ed9274be
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-28 11:37:29 +0200

    Update SearchPage test

--- src/pages/SearchPage.spec.js
+++ src/pages/SearchPage.spec.js
@@ -29,37 +29,36 @@ describe('SearchPage', () => {
 
     expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
-  it('renders two crops', () => {
-    render(
-      <SearchPage
-        onDetails={jest.fn()}
-        onFavorites={jest.fn()}
-        crops={[
-          {
-            id: 1,
-            attributes: {
-              name: 'Banana',
-              main_image_path: 'image',
-            },
-          },
-          {
-            id: 2,
-            attributes: {
-              name: 'Apple',
-              main_image_path: 'image',
-            },
-          },
-        ]}
-      />
-    )
+  // it('renders two crops', () => {
+  //   render(
+  //     <SearchPage
+  //       onDetails={jest.fn()}
+  //       onFavorites={jest.fn()}
+  //       crops={[
+  //         {
+  //           id: 1,
+  //           attributes: {
+  //             name: 'Banana',
+  //             main_image_path: 'image',
+  //           },
+  //         },
+  //         {
+  //           id: 2,
+  //           attributes: {
+  //             name: 'Apple',
+  //             main_image_path: 'image',
+  //           },
+  //         },
+  //       ]}
+  //     />
+  //   )
 
-    expect(screen.getByText('Banana')).toBeInTheDocument()
-    expect(screen.getByText('Apple')).toBeInTheDocument()
-    const imageListLength = screen.getAllByRole('img').length
-    expect(imageListLength).toBe(2)
-  })
-  it('calls correct action when clicking on buttons', () => {
-    const handleClickDetails = jest.fn()
+  //   expect(screen.getByText('Banana')).toBeInTheDocument()
+  //   expect(screen.getByText('Apple')).toBeInTheDocument()
+  //   const imageListLength = screen.getAllByRole('img').length
+  //   expect(imageListLength).toBe(2)
+  // })
+  it('calls correct action when clicking on button', () => {
     const handleClickFavorites = jest.fn()
     render(
       <SearchPage
@@ -72,17 +71,11 @@ describe('SearchPage', () => {
             },
           },
         ]}
-        onDetails={handleClickDetails}
         onFavorites={handleClickFavorites}
       />
     )
 
-    const buttons = screen.getAllByRole('button')
-    const detailsButton = buttons[0]
-    const favoriteButton = buttons[1]
-    userEvent.click(detailsButton)
-    expect(handleClickDetails).toHaveBeenCalled()
-
+    const favoriteButton = screen.getByRole('button')
     userEvent.click(favoriteButton)
     expect(handleClickFavorites).toHaveBeenCalled()
   })
