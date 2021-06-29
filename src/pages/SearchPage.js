54bc877eaf2f63d0e0ac9c2277acf84f95dd2d37
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-29 11:27:08 +0200

    Ajust styling

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -17,18 +17,22 @@ export default function SearchPage({
 }) {
   return (
     <Wrapper>
-      <SearchHeader>Harvestly</SearchHeader>
-      <Form onSubmit={handleSubmit}>
-        {/* Using div instead of label tag for easier styling */}
-        <div>What do you want to grow?</div>
-        <Input
-          type="text"
-          placeholder="Search for your favorite crops"
-          name="search"
-          aria-label="Search for crops"
-          autoComplete="off"
-        />
-      </Form>
+      <Background>
+        <Content>
+          <SearchHeader>Harvestly</SearchHeader>
+          <Form onSubmit={handleSubmit}>
+            {/* Using div instead of label tag for easier styling */}
+            <div>What do you want to grow?</div>
+            <Input
+              type="text"
+              placeholder="Search for your favorite crops"
+              name="search"
+              aria-label="Search for crops"
+              autoComplete="off"
+            />
+          </Form>
+        </Content>
+      </Background>
 
       <MyGardenButton onClick={onFavorites}>My Garden</MyGardenButton>
     </Wrapper>
@@ -44,27 +48,34 @@ export default function SearchPage({
 
 const Wrapper = styled.section`
   display: grid;
-  grid-template-rows: 48px auto 48px;
+  grid-template-rows: auto 48px;
   height: 100vh;
   width: 100vw;
+`
+const SearchHeader = styled(Header)`
+  text-align: center;
+  padding-bottom: 35px;
+`
+
+const Background = styled.div`
   background-image: url(${BackgroundMobile});
   background-size: cover;
   background-position: center;
   @media (min-width: 700px) {
     background-image: url(${BackgroundDesktop});
   }
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
-const SearchHeader = styled(Header)`
-  background: white;
-  text-align: center;
-  margin: 0;
-  border-radius: 5px;
+const Content = styled.div`
+  padding-bottom: 150px;
 `
 
 const Form = styled.form`
-  align-self: center;
+  /* align-self: center;
   justify-self: center;
-  padding-bottom: 150px;
+  padding-bottom: 150px; */
 `
 
 const Input = styled.input`
