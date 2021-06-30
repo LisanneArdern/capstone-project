4a125860acd496ec80831973912e99d82f1a970a
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-06-30 20:21:30 +0200

    Add colors to Global Style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,10 +1,23 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+ :root {
+   --color-basis: #fff;
+   --color-primary: #FEF5E7;
+   --color-secondary: #447F3D;
+   --color-border: #d3d3d3;
+   --color-shadow: #FFE5AA;
+   --color-light-shadow: rgba(0, 0, 0, 0.1);
+   --color-dark-green: #204028;
+   --color-text: #e4e4e4;
+ }
+
  * {
    box-sizing: border-box
  }
+
  body {
+   color: var(--color-dark-green);
    margin: 0;
    font-family: 'Roboto', sans-serif;
    font-size: 112.5%;
@@ -12,7 +25,7 @@ export default createGlobalStyle`
  }
 
  h1 {
-   font-size: 45px;
+   font-size: 35px;
  }
  h2 {
    font-size:20px;
