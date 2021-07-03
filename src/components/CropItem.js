723e58634df9d55683fe2899b4dee8569e1aa5b4
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-07-03 09:18:43 +0200

    Update CropItem test

--- src/components/CropItem.js
+++ src/components/CropItem.js
@@ -2,14 +2,14 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 
 CropItem.propTypes = {
-  name: PropTypes.string,
-  image: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 }
 
 export default function CropItem({ onClick, name, image }) {
   return (
-    <Wrapper role="button" onClick={onClick}>
+    <Wrapper onClick={onClick} role="button">
       <img src={image} alt="" width="100" height="66" />
       <h2>{name}</h2>
     </Wrapper>
