d5079a042f4a875809c25a1d4873356f7d8b76c4
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-07-05 10:19:45 +0200

    Update test for SearchPage

--- src/pages/SearchPage.spec.js
+++ src/pages/SearchPage.spec.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import SearchPage from './SearchPage'
 import { MemoryRouter } from 'react-router-dom'
 
@@ -7,56 +6,10 @@ describe('SearchPage', () => {
   it('renders an input field', () => {
     render(
       <MemoryRouter>
-        <SearchPage
-          onFavorites={jest.fn()}
-          setSearchTerm={jest.fn()}
-          onSubmit={jest.fn()}
-          crops={[
-            {
-              id: 1,
-              attributes: {
-                name: 'Banana',
-                main_image_path: 'image',
-              },
-            },
-            {
-              id: 2,
-              attributes: {
-                name: 'Apple',
-                main_image_path: 'image',
-              },
-            },
-          ]}
-        />
+        <SearchPage />
       </MemoryRouter>
     )
 
     expect(screen.getByRole('textbox')).toBeInTheDocument()
-  })
-
-  it('calls correct action when clicking on button', () => {
-    const handleClickFavorites = jest.fn()
-    render(
-      <MemoryRouter>
-        <SearchPage
-          crops={[
-            {
-              id: 1,
-              attributes: {
-                name: 'Banana',
-                main_image_path: 'image',
-              },
-            },
-          ]}
-          onSubmit={jest.fn()}
-          onFavorites={handleClickFavorites}
-          setSearchTerm={jest.fn()}
-        />
-      </MemoryRouter>
-    )
-
-    const favoriteButton = screen.getByRole('button')
-    userEvent.click(favoriteButton)
-    expect(handleClickFavorites).toHaveBeenCalled()
   })
 })
